24a9a019a46a1edd200666090dc5088e31f19cdd
Kunio Sawatani <devel@fathens.org>
2016-04-28 17:13:09 +0900

    AWS S3 への接続を実装

---
+++ src/services/aws/s3file.ts
@@ -0,0 +1,99 @@
+import {Logger} from '../../util/logging';
+import * as AWS from './aws';
+
+const logger = new Logger('S3File');
+
+export class S3File {
+    constructor(private bucketName: string, cred: AWS.Credential) {
+        logger.debug(() => `Creting S3: ${AWS.S3}`);
+        this.client = new AWS.S3({
+            accessKeyId: cred.accessKeyId,
+            secretAccessKey: cred.secretAccessKey,
+            region: cred.region
+        });
+    }
+
+    private client: AWS.S3;
+
+    private async invoke<R>(proc: (s3client) => AWS.Request): Promise<R> {
+        return AWS.requestToPromise<R>(proc(this.client));
+    }
+
+    async read(path: string): Promise<string> {
+        logger.debug(() => `Reading file: ${this.bucketName}:${path}`);
+        const res = await this.invoke<{ Body: number[] }>((s3) => s3.getObject({
+            Bucket: this.bucketName,
+            Key: path
+        }));
+        return String.fromCharCode.apply(null, res.Body);
+    }
+
+    async upload(path: string, blob: Blob): Promise<void> {
+        logger.debug(() => `Uploading file: ${this.bucketName}:${path}`);
+        await this.invoke((s3) => s3.putObject({
+            Bucket: this.bucketName,
+            Key: path,
+            Body: blob,
+            ContentType: blob.type
+        }));
+    }
+
+    async remove(path: string): Promise<void> {
+        logger.debug(() => `Removing file: ${this.bucketName}:${path}`);
+        await this.invoke((s3) => s3.deleteObject({
+            Bucket: this.bucketName,
+            Key: path
+        }));
+    }
+
+    async copy(src: string, dst: string): Promise<void> {
+        logger.debug(() => `Copying file: ${this.bucketName}:${src}=>${dst}`);
+        await this.invoke((s3) => s3.copyObject({
+            Bucket: this.bucketName,
+            CopySource: `${this.bucketName}/${src}`,
+            Key: dst
+        }));
+    }
+
+    async move(src: string, dst: string): Promise<void> {
+        await this.copy(src, dst);
+        await this.remove(src);
+    }
+
+    async list(path: string): Promise<Array<string>> {
+        logger.debug(() => `Listing up: ${path}`);
+        const res = await this.invoke<{ Contents: { Key: string }[] }>((s3) => s3.listObjects({
+            Bucket: this.bucketName,
+            Prefix: path
+        }));
+        return res.Contents.map((x) => x.Key);
+    }
+
+    async exists(path: string): Promise<boolean> {
+        logger.debug(() => `Checking exists: ${this.bucketName}:${path}`);
+        try {
+            const res = await this.invoke<{ ContentLength: number }>((s3) => s3.headObject({
+                Bucket: this.bucketName,
+                Key: path
+            }));
+            return res != null;
+        } catch (ex) {
+            logger.warn(() => `Error on checking exists: ${this.bucketName}:${path}`);
+            return false;
+        }
+    }
+
+    async url(path: string, expiresInSeconds: number): Promise<string> {
+        const s3: any = await this.client;
+        logger.debug(() => `Getting url of file: ${this.bucketName}:${path}`);
+        try {
+            return s3.getSignedUrl('getObject', {
+                Bucket: this.bucketName,
+                Key: path,
+                Expires: expiresInSeconds
+            });
+        } catch (ex) {
+            logger.warn(() => `Error on getting url: ${ex}`);
+        }
+    }
+}
