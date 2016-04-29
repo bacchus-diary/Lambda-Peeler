df8bf5a542900bfe5fb661cc46de82a36be5e7c6
Kunio Sawatani <devel@fathens.org>
2016-04-29 18:05:21 +0900

    spec 内の ts をコンパイル

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@ var gulp = require('gulp'),
 fs = require('fs'),
 del = require('del'),
 zip = require('gulp-zip'),
+babel = require('gulp-babel'),
 ts = require('gulp-typescript'),
+replace = require('gulp-replace'),
 jasmine = require('gulp-jasmine'),
 typings = require('gulp-typings'),
 webpack = require('gulp-webpack');
@@ -26,18 +28,28 @@ gulp.task('clean-typings', () => {
     return del(['typings']);
 });
 
-gulp.task('test', ['build'], () => {
-    return gulp.src('spec_bundle.js').pipe(jasmine());
+gulp.task('build-test', ['build'], () => {
+    const proj = ts.createProject('tsconfig.json');
+    return gulp.src('spec/**/*.ts')
+    .pipe(replace(/(import\s+.+\s+from\s+')\.\..*\/src\/.+(';\s*)/g, '$1../main_bundle$2'))
+    .pipe(ts(proj))
+    .pipe(babel({
+        presets: ['es2015', 'stage-0']
+    })).pipe(gulp.dest('spec/'));
+});
+
+gulp.task('test', ['build-test'], () => {
+    return gulp.src('spec/**/*.js').pipe(jasmine());
 });
 
 gulp.task('pack', ['test'], () => {
     const exclude = /aws\-sdk/;
     const externals = require('./webpack.config.js').externals;
-    const sources = fs.readdirSync('./node_modules')
+    const sources = ['./*_bundle.js', './spec/**', '!./spec/**/*.ts'];
+    fs.readdirSync('./node_modules')
     .filter((x) => !exclude.test(x))
     .filter((x) => externals.some((y) => y.test(x)))
-    .map((x) => `./node_modules/${x}/**`);
-    sources.push('./*_bundle.js');
+    .forEach((x) => sources.push(`./node_modules/${x}/**`));
     console.log(`Packing: ${JSON.stringify(sources, null, 4)}`);
     return gulp.src(sources, { base: './' })
     .pipe(zip('main.zip'))
