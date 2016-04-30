fe66f0232b47a4c9f1b58a776fbd5be245a87851
Kunio Sawatani <devel@fathens.org>
2016-04-30 10:35:15 +0900

    spec 内からログを出力

--- spec/sample_spec.ts
+++ spec/sample_spec.ts
@@ -1,8 +1,12 @@
 import {Main} from '../src/index';
+import {Logger} from '../src/util/logging';
+
+const logger = new Logger('SampleSpec');
 
 describe('SampleText', () => {
     it('サンプルは同じ', () => {
         const main = new Main('AA1');
+        logger.debug(() => `Main: ${main}`);
         expect(main.sample).toBe('AA1');
     });
 });
