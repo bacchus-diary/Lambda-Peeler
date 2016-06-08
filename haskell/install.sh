8f93c03b329db1e9cf495b72b0699c61d30624af
Kunio Sawatani <devel@fathens.org>
2016-06-08 23:26:49 +0900

    ldd からのライブラリの検知方法を変更
    root が行うので /home にはなりません。

--- haskell/install.sh
+++ haskell/install.sh
@@ -5,5 +5,8 @@ target_dir=$1
 find . -not -path '*/\.*' -type f -name '*.so' | while read file
 do
     cp -vu $file $target_dir
-    cp -vu $(ldd $file | awk '{print $(NF-1)}' | grep /home) $target_dir
+    ldd $file | awk '{print $(NF-1)}' | grep /.stack | while read lib
+    do
+        cp -vu $lib $target_dir
+    done
 done
