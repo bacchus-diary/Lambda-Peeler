727fc27e70f37c022f70551c8b034c2de1c4325e
Kunio Sawatani <devel@fathens.org>
2016-06-08 20:38:42 +0900

    Haskell の so は必要な時だけコピー

--- haskell/install.sh
+++ haskell/install.sh
@@ -4,6 +4,6 @@ target_dir=$1
 
 find . -not -path '*/\.*' -type f -name '*.so' | while read file
 do
-    cp -vf $file $target_dir
-    cp -vf $(ldd $file | awk '{print $(NF-1)}' | grep /home) $target_dir
+    cp -vu $file $target_dir
+    cp -vu $(ldd $file | awk '{print $(NF-1)}' | grep /home) $target_dir
 done
