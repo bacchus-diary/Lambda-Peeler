fcdecdeaab0544d39f2cf9335fc170401be6fbe4
Kunio Sawatani <devel@fathens.org>
2016-06-08 23:53:41 +0900

    stderr はカットしない

--- lambda_update.sh
+++ lambda_update.sh
@@ -7,14 +7,14 @@ AWS_S3_FILEPATH=${2#*/}
 ZIP_NAME=${2##*/}
 
 json() {
-  python -c "import sys,json; print(json.loads(sys.stdin.read())['$1'])" 2> /dev/null
+  python -c "import sys,json; print(json.loads(sys.stdin.read())['$1'])"
 }
 
-PRE=$(aws lambda get-alias --function-name $AWS_LAMBDA_FUNC_NAME --name $AWS_LAMBDA_ALIAS_NAME 2> /dev/null | json FunctionVersion)
+PRE=$(aws lambda get-alias --function-name $AWS_LAMBDA_FUNC_NAME --name $AWS_LAMBDA_ALIAS_NAME | json FunctionVersion)
 
-aws s3 cp $ZIP_NAME s3://$AWS_S3_BUCKET/$AWS_S3_FILEPATH > /dev/null 2>&1 || exit 1
+aws s3 cp $ZIP_NAME s3://$AWS_S3_BUCKET/$AWS_S3_FILEPATH > /dev/null || exit 1
 
-VERSION=$(aws lambda update-function-code --function-name $AWS_LAMBDA_FUNC_NAME --s3-bucket $AWS_S3_BUCKET --s3-key $AWS_S3_FILEPATH --publish 2> /dev/null | json Version)
+VERSION=$(aws lambda update-function-code --function-name $AWS_LAMBDA_FUNC_NAME --s3-bucket $AWS_S3_BUCKET --s3-key $AWS_S3_FILEPATH --publish | json Version)
 [ -z $VERSION ] && exit 1
 
 CU='update'
