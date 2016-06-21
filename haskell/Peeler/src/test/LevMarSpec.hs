a275d65c6785aa7026642fe30a14bd4cce234dc9
Kunio Sawatani <devel@fathens.org>
2016-06-21 11:46:16 +0900

    Haskell と C のインデントを 4 に統一

--- haskell/Peeler/src/test/LevMarSpec.hs
+++ haskell/Peeler/src/test/LevMarSpec.hs
@@ -5,11 +5,11 @@ import LevMar
 
 spec :: Spec
 spec = do
-  describe "Levenberg Marquardt" $ do
-    it "dif" $ do
-      result <- levmarDif 3 calc 1000 [0, 0]
-      result `shouldBe` [0.7999999999996789, 0.1999999999999391]
-      where
-        points = [(-3.1, 0.18), (0.3, 0.86), (1.2, 1.04)]
-        calc params = map (diff params) points
-        diff [p0, p1] (x, y) = p1 * x + p0 - y
+    describe "Levenberg Marquardt" $ do
+        it "dif" $ do
+            result <- levmarDif 3 calc 1000 [0, 0]
+            result `shouldBe` [0.7999999999996789, 0.1999999999999391]
+            where
+                points = [(-3.1, 0.18), (0.3, 0.86), (1.2, 1.04)]
+                calc params = map (diff params) points
+                diff [p0, p1] (x, y) = p1 * x + p0 - y
