0aa42dbe4a790fe04aad74e86bb1bee5335a5d30
Kunio Sawatani <devel@fathens.org>
2016-06-22 16:55:59 +0900

    Haskell の実行テスト用ファイルを S3 から

--- src/spec/haskell/haskell_spec.ts
+++ src/spec/haskell/haskell_spec.ts
@@ -1,6 +1,8 @@
 import * as spec from "../_specs";
+import {S3File} from "../../services/aws/s3file";
 import {Logger} from "../../util/logging";
 
+const path = require("path");
 const ffi = require("ffi");
 const ref = require("ref");
 
@@ -11,15 +13,18 @@ const C_string = ref.types.CString;
 const peeler = ffi.Library("libHSPeeler", {
     "start": [C_string, [C_string]]
 });
+const s3 = new S3File("build-config");
 
 export const specifications = spec.describe({
     "Haskell": {
-        "start": () => {
-            const original = "Some text";
-            const result = peeler.start(original);
-            logger.debug(() => `Result of dif: ${JSON.stringify(result)}`);
+        "start": async () => {
+            const s3path = "bacchus-diary/Lambda-Peeler/test_data/pickled_cucumbers.mp4";
+            const filepath = path.resolve(`/tmp/${path.basename(s3path)}`);
+            await s3.download(s3path, filepath);
+            const result = peeler.start(filepath);
+            logger.debug(() => `Result of start: ${JSON.stringify(result)}`);
 
-            spec.expect(result).must_be(original);
+            spec.expect(result).must_be(filepath);
         }
     }
 });
