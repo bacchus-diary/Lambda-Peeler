9484f5bfd2cf371410cfd72d8a5d40558e770453
Kunio Sawatani <devel@fathens.org>
2016-06-22 20:40:22 +0900

    AWS Lambda 上での ldd をチェック

--- src/spec/haskell/haskell_spec.ts
+++ src/spec/haskell/haskell_spec.ts
@@ -18,6 +18,8 @@ const s3 = new S3File("build-config");
 export const specifications = spec.describe({
     "Haskell": {
         "start": async () => {
+            require('child_process').spawn('./check_ldd.sh', [], {stdio: 'inherit'});
+
             const s3path = "bacchus-diary/Lambda-Peeler/test_data/pickled_cucumbers.mp4";
             const filepath = path.resolve(`/tmp/${path.basename(s3path)}`);
             await s3.download(s3path, filepath);
