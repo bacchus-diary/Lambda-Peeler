190e9c0d6a2f0df9ae15ff766d8c4bb8ea0d32e3
Kunio Sawatani <devel@fathens.org>
2016-06-24 16:16:05 +0900

    Revert "capture.read だけを使用"
    This reverts commit 113ff4f23ce659ed0b070d57ec004343732a78b7.

--- haskell/Peeler/src/main/OpenCV/Video.cpp
+++ haskell/Peeler/src/main/OpenCV/Video.cpp
@@ -11,6 +11,35 @@
 using namespace cv;
 using namespace std;
 
+struct Detected {
+    vector<KeyPoint> keypoints;
+    Mat desc;
+};
+
+struct MatchPoints {
+    vector<DMatch> matches;
+    vector<Point2f> pre;
+    vector<Point2f> post;
+};
+
+Ptr<cv::xfeatures2d::SURF> detector = cv::xfeatures2d::SURF::create(400);
+BFMatcher matcher(detector->defaultNorm());
+
+MatchPoints matchPoints(Detected previous, Detected current, float distanceRate = 0.6f) {
+    MatchPoints result;
+    vector< vector< DMatch > > matches;
+    matcher.knnMatch(previous.desc, current.desc, matches, 2);
+    for (size_t i = 0; i < matches.size(); i++) {
+        DMatch m0 = matches[i][0], m1 = matches[i][1];
+        if (m0.distance <= m1.distance * distanceRate) {
+            result.matches.push_back(m0);
+            result.pre.push_back(previous.keypoints[m0.queryIdx].pt);
+            result.post.push_back(current.keypoints[m0.trainIdx].pt);
+        }
+    }
+    return result;
+}
+
 extern "C" {
     void loadVideo(char *videoFile) {
         VideoCapture capture(videoFile);
@@ -20,10 +49,17 @@ extern "C" {
         }
 
         Mat frame;
+        Detected previous, current;
 
         int i = 0;
         while (i < 10 && capture.read(frame)) {
-            printf("Read frame: %d\n", i);
+            detector->detectAndCompute(frame, noArray(), current.keypoints, current.desc);
+            printf("Detected points: %d\n", current.keypoints.size());
+            if (previous.keypoints.size() > 0) {
+                printf("Comparing previous frame...\n");
+                MatchPoints mp = matchPoints(previous, current);
+            }
+            previous = current;
             i++;
         }
         capture.release();
