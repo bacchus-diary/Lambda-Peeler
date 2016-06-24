ba3bfbf4e73dc26e3826ce500405af2a4a421650
Kunio Sawatani <devel@fathens.org>
2016-06-24 16:22:42 +0900

    C++ から Haskell への呼び出しを復活

--- haskell/Peeler/src/main/OpenCV/Video.cpp
+++ haskell/Peeler/src/main/OpenCV/Video.cpp
@@ -13,34 +13,8 @@
 using namespace cv;
 using namespace std;
 
-struct Detected {
-    vector<KeyPoint> keypoints;
-    Mat desc;
-};
-
-struct MatchPoints {
-    vector<DMatch> matches;
-    vector<Point2f> pre;
-    vector<Point2f> post;
-};
-
 Ptr<cv::xfeatures2d::SURF> detector = cv::xfeatures2d::SURF::create(400);
 BFMatcher matcher(detector->defaultNorm());
-
-MatchPoints matchPoints(Detected previous, Detected current, float distanceRate = 0.6f) {
-    MatchPoints result;
-    vector< vector< DMatch > > matches;
-    matcher.knnMatch(previous.desc, current.desc, matches, 2);
-    for (size_t i = 0; i < matches.size(); i++) {
-        DMatch m0 = matches[i][0], m1 = matches[i][1];
-        if (m0.distance <= m1.distance * distanceRate) {
-            result.matches.push_back(m0);
-            result.pre.push_back(previous.keypoints[m0.queryIdx].pt);
-            result.post.push_back(current.keypoints[m0.trainIdx].pt);
-        }
-    }
-    return result;
-}
 
 extern "C" {
     void loadVideo(char *videoFile) {
@@ -51,18 +25,14 @@ extern "C" {
         }
 
         Mat frame;
-        Detected previous, current;
+        vector<KeyPoint> keypoints;
+        Mat desc;
 
         int i = 0;
         while (i < 10 && capture.read(frame)) {
-            detector->detectAndCompute(frame, noArray(), current.keypoints, current.desc);
-            printf("Detected points: %d\n", current.keypoints.size());
-            if (previous.keypoints.size() > 0) {
-                printf("Comparing previous frame...\n");
-                MatchPoints mp = matchPoints(previous, current);
-                compareKeypoints(i);
-            }
-            previous = current;
+            detector->detectAndCompute(frame, noArray(), keypoints, desc);
+            printf("Detected points: %d\n", keypoints.size());
+            compareKeypoints(i);
             i++;
         }
         capture.release();
