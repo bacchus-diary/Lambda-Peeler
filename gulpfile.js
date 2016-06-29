ea32d4e9f5cace8d099bc9ef52982d2d43555371
Kunio Sawatani <devel@fathens.org>
2016-06-29 10:42:18 +0900

    contrib_world を使用

--- gulpfile.js
+++ gulpfile.js
@@ -70,7 +70,7 @@ gulp.task('haskell', shell.task([
 ]));
 
 gulp.task('only_c', shell.task([
-    "g++ -L/var/task/lib -I/var/task/include haskell/Peeler/src/main/OpenCV/only_c.cpp -llevmar -lopencv_aruco -lopencv_bgsegm -lopencv_bioinspired -lopencv_calib3d -lopencv_ccalib -lopencv_core -lopencv_datasets -lopencv_dnn -lopencv_dpm -lopencv_face -lopencv_features2d -lopencv_flann -lopencv_fuzzy -lopencv_highgui -lopencv_imgcodecs -lopencv_imgproc -lopencv_line_descriptor -lopencv_ml -lopencv_objdetect -lopencv_optflow -lopencv_photo -lopencv_plot -lopencv_reg -lopencv_rgbd -lopencv_saliency -lopencv_shape -lopencv_stereo -lopencv_stitching -lopencv_structured_light -lopencv_superres -lopencv_surface_matching -lopencv_text -lopencv_tracking -lopencv_videoio -lopencv_video -lopencv_videostab -lopencv_xfeatures2d -lopencv_ximgproc -lopencv_xobjdetect -lopencv_xphoto -o haskell/Peeler/only_c"
+    "g++ -L/var/task/lib -I/var/task/include haskell/Peeler/src/main/OpenCV/only_c.cpp -llevmar -lopencv_aruco -lopencv_bgsegm -lopencv_bioinspired -lopencv_calib3d -lopencv_ccalib -lopencv_core -lopencv_datasets -lopencv_dnn -lopencv_dpm -lopencv_face -lopencv_features2d -lopencv_flann -lopencv_fuzzy -lopencv_highgui -lopencv_imgcodecs -lopencv_imgproc -lopencv_line_descriptor -lopencv_ml -lopencv_objdetect -lopencv_optflow -lopencv_photo -lopencv_plot -lopencv_reg -lopencv_rgbd -lopencv_saliency -lopencv_shape -lopencv_stereo -lopencv_stitching -lopencv_structured_light -lopencv_superres -lopencv_surface_matching -lopencv_text -lopencv_tracking -lopencv_videoio -lopencv_video -lopencv_videostab -lopencv_contrib_world -lopencv_ximgproc -lopencv_xobjdetect -lopencv_xphoto -o haskell/Peeler/only_c"
 ]));
 
 gulp.task('test-only', [], (cb) => {
