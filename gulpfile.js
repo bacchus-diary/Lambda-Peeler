e4ce085cf422731f21f773ba8a0a5e5af64f5070
Kunio Sawatani <devel@fathens.org>
2016-07-01 14:44:19 +0900

    Revert "executable と library をまとめてしまう"
    This reverts commit 9a81ac7be2d9e21d62482f730a791d4c6130a8c2.

--- gulpfile.js
+++ gulpfile.js
@@ -65,7 +65,7 @@ gulp.task('inject-tests', (cb) => {
 });
 
 gulp.task('haskell', shell.task([
-    "cd haskell && stack --install-ghc --allow-different-user install",
+    "cd haskell && stack --install-ghc --allow-different-user test",
     "haskell/install.sh /var/task/lib"
 ]));
 
