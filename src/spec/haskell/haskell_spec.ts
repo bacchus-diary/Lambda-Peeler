d00f37b539d6ef987448e218723b4ff439227f5a
Kunio Sawatani <devel@fathens.org>
2016-07-01 18:26:16 +0900

    haskell からの戻り値をチェック

--- src/spec/haskell/haskell_spec.ts
+++ src/spec/haskell/haskell_spec.ts
@@ -21,9 +21,12 @@ export const specifications = spec.describe({
             const s3path = "bacchus-diary/Lambda-Peeler/test_data/pickled_cucumbers.mp4";
             const filepath = path.resolve(`/tmp/${path.basename(s3path)}`);
             await s3.download(s3path, filepath);
+
             logger.debug(() => `Loading video: ${filepath}`);
             const result = peeler.start(filepath);
             logger.debug(() => `Result of start: ${JSON.stringify(result)}`);
+
+            spec.expect(result).must_be(filepath);
         }
     }
 });
