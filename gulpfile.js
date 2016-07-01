9a81ac7be2d9e21d62482f730a791d4c6130a8c2
Kunio Sawatani <devel@fathens.org>
2016-07-01 12:24:37 +0900

    executable と library をまとめてしまう

--- gulpfile.js
+++ gulpfile.js
@@ -65,7 +65,7 @@ gulp.task('inject-tests', (cb) => {
 });
 
 gulp.task('haskell', shell.task([
-    "cd haskell && stack --install-ghc --allow-different-user test",
+    "cd haskell && stack --install-ghc --allow-different-user install",
     "haskell/install.sh /var/task/lib"
 ]));
 
